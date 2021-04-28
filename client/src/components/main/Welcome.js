e52e71eef9b6141d529948bd103df072b97c7f19
JeongYoon-L <Jeongyoon.lee.1@stonybrook.edu>
2021-04-28 21:52:26 +0900

    initial commit

---
+++ client/src/components/main/Welcome.js
@@ -0,0 +1,14 @@
+import React            from 'react';
+
+
+const Welcome = (props) => {
+    return (
+        
+        <div className='welcome'  >
+            Welcome To The World Data Mapper
+        </div>
+        
+    );
+};
+
+export default Welcome;
---
+++ models/user-model.js
@@ -0,0 +1,30 @@
+const { model, Schema, ObjectId } = require('mongoose');
+
+const userSchema = new Schema(
+	{
+		_id: {
+			type: ObjectId,
+			required: true
+		},
+		Name: {
+			type: String,
+			required: true
+		},
+		initials: {
+			type: String,
+			required: true
+		},
+		email: {
+			type: String,
+			required: true
+		},
+		password: {
+			type: String,
+			required: true
+		}
+	},
+	{ timestamps: true }
+);
+
+const User = model('User', userSchema);
+module.exports = User;
